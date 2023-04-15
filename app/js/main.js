c4704cf862f9226111faf7be973432f719f64684
unknown <marselina88@list.ru>
2023-04-15 09:28:07 +0300

    вывод картинок в about из json

--- app/js/main.js
+++ app/js/main.js
@@ -418,8 +418,35 @@ __webpack_require__.r(__webpack_exports__);
 swiper__WEBPACK_IMPORTED_MODULE_0__["default"].use([swiper__WEBPACK_IMPORTED_MODULE_0__.Navigation, swiper__WEBPACK_IMPORTED_MODULE_0__.Thumbs]);
 const modalSlider = document.querySelector(".modalSwiper");
 const modalSlider2 = document.querySelector(".modalSwiper2");
-console.log(modalSlider);
-console.log(modalSlider2);
+const imgList = document.querySelector('.about-interior__images');
+let prodQuantity = 3;
+let dataLength = null;
+const idImg = document.querySelectorAll('.about-interior__img');
+if (imgList) {
+  const loadImg = function () {
+    let quantity = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+    fetch('../data/gallery.json').then(response => {
+      return response.json();
+    }).then(data => {
+      dataLength = data.length;
+      imgList.innerHTML = '';
+      //console.log(imgList);
+      for (let i = 0; i < dataLength; i++) {
+        if (i < quantity) {
+          let item = data[i];
+          let id = item.id;
+          if (id == 1) console.log(id);
+          //console.log(id);
+          imgList.innerHTML += `<li class="${id === 1 ? 'about-interior__img-left' : ''} about-interior-img about-interior__img wow animate__fadeInLeft btnModalFirst" data-graph-path="modal-first" data-wow-offset="300" data-wow-delay=".1s" data-id="${item.id}">
+          <img src="${item.mainImage}" alt="interior">
+          <div class="about__overlay"></div>
+        </li>`;
+        }
+      }
+    });
+  };
+  loadImg(prodQuantity);
+}
 var swiper = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](modalSlider, {
   spaceBetween: 10,
   slidesPerView: 4,
--- src/js/components/modal-slider.js
+++ src/js/components/modal-slider.js
@@ -2,8 +2,58 @@ import Swiper, { Navigation, Thumbs} from 'swiper';
 Swiper.use([Navigation, Thumbs]);
 const modalSlider = document.querySelector(".modalSwiper");
 const modalSlider2 = document.querySelector(".modalSwiper2");
-console.log(modalSlider);
-console.log(modalSlider2);
+const imgList = document.querySelector('.about-interior__images');
+let prodQuantity = 3;
+let dataLength = null;
+const idImg = document.querySelectorAll('.about-interior__img');
+
+
+
+
+
+if(imgList) {
+  const loadImg =(quantity = 3) => {
+    fetch('../data/gallery.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+      dataLength = data.length;
+      imgList.innerHTML = '';
+      //console.log(imgList);
+      for (let i = 0; i< dataLength; i++) {
+        if (i < quantity) {
+          let item = data[i];
+          let id = item.id;
+          if (id == 1)
+        console.log(id)
+          //console.log(id);
+          imgList.innerHTML += `<li class="${id === 1 ? 'about-interior__img-left' : ''} about-interior-img about-interior__img wow animate__fadeInLeft btnModalFirst" data-graph-path="modal-first" data-wow-offset="300" data-wow-delay=".1s" data-id="${item.id}">
+          <img src="${item.mainImage}" alt="interior">
+          <div class="about__overlay"></div>
+        </li>`;}
+
+      }
+    })
+
+
+  }
+  loadImg(prodQuantity);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var swiper = new Swiper(modalSlider, {
   spaceBetween: 10,
