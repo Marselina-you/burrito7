b83df432306057b9976b31e298ef108bc662fc74
Marselina-you <marselina88@list.ru>
2023-06-02 19:44:54 +0300

    подделки под WordPress

--- src/js/components/burger-menu.js
+++ src/js/components/burger-menu.js
@@ -2,7 +2,8 @@ import WOW from 'wowjs';
 
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.header__nav');
-const items = document.querySelectorAll('.nav__item');
+const navwrap = document.querySelector('.nav-wrap');
+const list = document.querySelector('.nav__list');
 const wrap = document?.querySelector('.burger-wrap');
 
 
@@ -11,8 +12,10 @@ const wrap = document?.querySelector('.burger-wrap');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('burger--active');
-  menu.classList.toggle('header__nav--active');
+ // menu.classList.toggle('header__nav');
+  navwrap.classList.toggle('nav-wrap--active');
   wrap?.classList.toggle('burger-wrap--close');
+  list.classList.toggle('nav__list--tablet')
 
 
 })
